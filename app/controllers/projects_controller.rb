b8b4ef75eb043749629dec407e5ef8e59dd5060f
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-16 07:53:53 +0300

    created a permit for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,6 +69,9 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.fetch(:project, {})
+      params.fetch(:project).permit(:name,:picture,:link) 
     end
 end
+
+
+ 
