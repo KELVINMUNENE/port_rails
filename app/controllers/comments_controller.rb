d2cf290be5a36a17f61c6e6e57e0deae9ed21b38
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-16 14:46:36 +0300

    chris merged user and post id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,28 @@
 class CommentsController < ApplicationController
+	before_action :get_user, only: [:create,:new]
 	def new
 	@comment = Comment.new
-	@post = Post.find(params[:id])
+	end
+	def create
+		@comment = Comment.new(comment_params)
+		if @comment.save
+			flash[:notice] = "comment saved successfully"
+			redirect_to post_path(params[:post_id])
+		else
+			flash[:notice] = "comment not saved"
+			redirect_to post_path(params[:post_id])
+		end
 		
+	end
+	private
+	def comment_params
+		user = {user_id:@user.id}
+		post = {post_id:params[:post_id]}
+		params.require(:comment).permit(:description, :post_id).merge(user).merge(post)
+
+	end
+	def get_user
+		@user = current_user
+		@post = Post.find(params[:post_id])
 	end
 end
