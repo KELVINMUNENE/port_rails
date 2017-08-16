365b9cdab3c2b1de754f40e4762d31817deba1f9
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-16 07:22:53 +0300

    run a migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816041447) do
+ActiveRecord::Schema.define(version: 20170816042005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "picture"
+    t.string "link"
+  end
 
   create_table "skills", force: :cascade do |t|
     t.string "name"
