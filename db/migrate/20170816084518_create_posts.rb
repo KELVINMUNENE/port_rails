b12701a83296dec882ab8e5604ca8c44a29d78a4
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-16 11:50:00 +0300

    create columns for posts

--- db/migrate/20170816084518_create_posts.rb
+++ db/migrate/20170816084518_create_posts.rb
@@ -1,9 +1,13 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-    	
+	t.column :title, :string
+	t.column :description, :string	
 
-      t.timestamps
+    t.timestamps
     end
   end
 end
+
+
+	
