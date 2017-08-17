376aa6e61ccf3dd37ce1679c1ab16692786ded91
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-17 10:14:03 +0300

    passed user confirmation to assign notice if successful

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,19 @@
-users_controller.rb
+class UsersController < ApplicationController
+		def show
+			@user = User.find(params[:id])
+		end
+
+		def new
+			@user = user.new
+		end
+
+		def create
+			@user = User.find(params[:user])
+			if @user.save
+				UserMail.signup_confirmation(@user).deliver
+				redirect_to @user, notice: "signed up successful"
+			else 
+				render :new
+			end
+		end	
+end
