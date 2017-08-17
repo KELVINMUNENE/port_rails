2168ce607818854a321074113baddc032be69ea6
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-17 10:03:21 +0300

    added a confirmation message to user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,14 @@
 class UserMailer < ApplicationMailer
+		default from : "kelvinm@gmail.com"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.signup_confirmation.subject
-  #
-  def signup_confirmation
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
-  end
+	  # Subject can be set in your I18n file at config/locales/en.yml
+	  # with the following lookup:
+	  #
+	  #   en.user_mailer.signup_confirmation.subject
+	  #
+	  def signup_confirmation(user)
+	  	@user = user
+	  
+	  mail to:user.email, subject:"sign up confirmation"
+	end
 end
