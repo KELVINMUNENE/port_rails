ec152f230e126bd3f0f17e14fca509c228eed03c
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-21 14:38:15 +0300

    run a migration

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -69,6 +69,6 @@ class RecommendationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def recommendation_params
-      params.fetch(:recommendation, {})
+      params.fetch(:recommendation).permit(:recommendation) 
     end
 end
--- db/migrate/20170821113150_create_recommendations.rb
+++ db/migrate/20170821113150_create_recommendations.rb
@@ -1,8 +1,6 @@
 class CreateRecommendations < ActiveRecord::Migration[5.1]
   def change
     create_table :recommendations do |t|
-
-      t.timestamps
     end
   end
 end
