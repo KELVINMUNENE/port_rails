fb7d23a571f3910dd01b8b345aac564959bcb8dc
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-23 09:09:02 +0300

    created a description for projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822133344) do
+ActiveRecord::Schema.define(version: 20170823060743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170822133344) do
     t.string "name"
     t.string "picture"
     t.string "link"
+    t.string "description"
   end
 
   create_table "recommendations", force: :cascade do |t|
