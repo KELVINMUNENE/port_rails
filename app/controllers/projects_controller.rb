3a8d40695874637b88a5b6f2f0bb648f3fb7b080
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-23 09:42:00 +0300

    permitted description to display

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,7 +69,7 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.fetch(:project).permit(:name,:picture,:link) 
+      params.fetch(:project).permit(:name, :description) 
     end
 end
 
